20b45602ef0bf273a7de3f4f694c138e0a2666fc
BillyBag2 <bagshawwilliam@gmail.com>
2023-01-01 14:11:18 +0000

    shell.sh now uses older k80 compatable docker explicitly. shell.6.1.sh is for newer p4 compatable shell.

--- shell.6.1.sh
+++ shell.6.1.sh
@@ -4,4 +4,4 @@ docker run -it --rm --gpus all\
  -v `pwd`/build:/build\
  -v `pwd`/greggs_data/data:/data\
  -v `pwd`/greggs_data/test:/test\
- billybag2/darknet-yolo4:k80
+ billybag2/darknet-yolo4:c6.1
