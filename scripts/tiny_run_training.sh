aed5148d8079534ecd6b08cec3d63eb60a74bdb4
BillyBag2 <bagshawwilliam@gmail.com>
2023-01-05 20:04:10 +0000

    Add -iou_thresh 0.75 to tiny training scrits to make mAP measurment more sesative.

--- scripts/tiny_run_training.sh
+++ scripts/tiny_run_training.sh
@@ -6,4 +6,4 @@ set -e
 # It expects a /build directory and a /data directory.
 
 cd /root/darknet
-./darknet detector train /build/obj.data /build/yolo4-tiny-greggs.cfg /build/yolov4-tiny.conv.29 -dont_show -map
+./darknet detector train /build/obj.data /build/yolo4-tiny-greggs.cfg /build/yolov4-tiny.conv.29 -dont_show -map -iou_thresh 0.75
